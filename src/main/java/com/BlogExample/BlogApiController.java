79aff8e858afec0bced5e21f12c748afc82f722d
Woomin Ko <woomin422@gmail.com>
2025-01-30 18:21:43 +0900

    done

--- src/main/java/com/BlogExample/BlogApiController.java
+++ src/main/java/com/BlogExample/BlogApiController.java
@@ -1,14 +1,14 @@
 package com.BlogExample;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
-@Controller
+@RequiredArgsConstructor
+@RestController
 public class BlogApiController {
     private final BlogService blogService;
 
@@ -17,8 +17,8 @@ public class BlogApiController {
             Article savedArticle = blogService.save(request);
             return ResponseEntity.status(HttpStatus.CREATED)
                     .body(savedArticle);
-        }
-
     }
 
 }
+
+
--- src/main/java/com/BlogExample/BlogService.java
+++ src/main/java/com/BlogExample/BlogService.java
@@ -1,8 +1,16 @@
 package com.BlogExample;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class BlogService {
-    private BlogRepository blogRepository;
+    private final BlogRepository blogRepository;
     public Article save(AddArticleRequest request){
+
         return blogRepository.save(request.toEntity());
     }
+
+
 }
