be65a908c44ce9659d1655bf7a65f826c2246dd4
Woomin Ko <woomin422@gmail.com>
2025-01-30 19:07:27 +0900

    done

--- src/main/java/com/BlogExample/BlogApiController.java
+++ src/main/java/com/BlogExample/BlogApiController.java
@@ -3,12 +3,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
+@CrossOrigin(origins = "*")
 public class BlogApiController {
     private final BlogService blogService;
 
